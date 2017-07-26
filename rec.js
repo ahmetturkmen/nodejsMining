f437efd54fa2f02d16f032a0162eb7b81e3ec24a
fsahin3458 <fsahin3458@gmail.com>
2017-07-26 10:28:34 +0300

    sa

---
+++ rec.js
@@ -0,0 +1,39 @@
+const redis = require('redis');
+const recClient = redis.createClient({host:'localhost',port:6379});
+
+let users ={
+        "Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+         "Bill":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
+         "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
+         "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
+         "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
+         "Jordyn":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
+         "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+         "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+      };
+
+
+  
+function manhattan(user1, user2){
+        let distance =0;
+        for (let key in user1) {
+        if (key in user2) {
+          distance+=Math.abs(user1[key]-user2[key])
+        }
+    }
+   return 'The manhattan distance between them : '+distance;
+}
+
+
+console.log(manhattan(users['Hailey'],users['Veronica']));
+
+function euclidean(rating1,rating2){
+    let distance=0;
+    for(var key in rating1){
+        if(key in rating2)
+            distance += ((Math.pow((rating1[key]-rating2[key]),2)));
+           
+    }
+    return Math.sqrt(distance)
+}
+
